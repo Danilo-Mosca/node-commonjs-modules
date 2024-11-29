1fd750d835f7fa5b889264516093cb99e3ab47c8
Danilo-Mosca <danilo.mosca85@gmail.com>
2024-11-29 15:29:38 +0100

    added all js files. Created function hobbies.js and exported it

---
+++ hobbies.js
@@ -0,0 +1,12 @@
+function objectReturn(firstName, lastName) {
+    //ritorno l'oggetto
+    return {
+        firstName,
+        lastName,
+    }
+}
+// console.log("Oggetto: ", objectReturn("Nome", "Cognome"));
+
+// Esporto la funzione dal file:
+module.exports = { objectReturn };
+
